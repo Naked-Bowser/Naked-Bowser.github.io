ffc399ba2829da43c62bb06747fe928ee6320419
Nake-Bowser <majortheory@icloud.com>
2026-04-25 21:18:54 -0400

    SMASHER

--- Javascript/script.js
+++ Javascript/script.js
@@ -1,101 +1,48 @@
-/*********************************************************
- *
- *	Author: Maramkhah, Shayon
- *	Date: January 11th, 2026
- *	Updated: March 20th, 2026, 1:12pm
- *	FileType: script.js
- *	Description: 
- *		Javascript file for dotsmasher. You have 734x430 pixels which is 315,620 sqaure pixel space.
- *
- *		Actions:
- *			1) Intergrate Email correspondence.
- *			2) Can you query google with Fetch API?
- *			3) Orient dot smasher.
- *		Commands:
- *			querySelector();
- *			getBoundingClientRect();
- *			classList.add
- *			createElement
- *			style.display
- *			document.body.appendChild();
- *			...
- * **/
+const getCont = document.querySelector(".FlexContainer");
 
-//Global Variables
-const points = document.querySelector(".Points");
+const getDot = document.querySelector(".Dot");
 
-const dot = document.querySelector(".Dot");
-const dotRange = dot.getBoundingClientRect();//Make sure dot is less than or equal to bounds!
-const containerBody = document.querySelector("body");//container body
-const rect = dot.getBoundingClientRect();//Container bounds
+const getParameters = getCont.getBoundingClientRect();
 
-const obj = [];//rect bounds
-for(const key in rect){
-	if(typeof rect[key] !== "function"){
-		obj.push(`${rect[key]}`);
-	}
-}
+const getParameters1 = getDot.getBoundingClientRect();
 
+console.log("FlexContainer");
+console.log("X value",getParameters.x);
+console.log("Y value",getParameters.y);
+console.log("Top value",getParameters.top);
+console.log("left value",getParameters.left);
+console.log("Width value",getParameters.width);
+console.log("Height value",getParameters.height);
+console.log("Right value",getParameters.right);
+console.log("Bottom value",getParameters.bottom);
+console.log("Dot")
+console.log("X value",getParameters1.x);
+console.log("Y value",getParameters1.y);
+console.log("Top value",getParameters1.top);
+console.log("Left value",getParameters1.left);
+console.log("Width value",getParameters1.width);
+console.log("Height value",getParameters1.height);
+console.log("Right value",getParameters1.right);
+console.log("Bottom value",getParameters1.bottom);
 
-const obj1 = [];//dot range bounds
-for(const key in dotRange){
-	if(typeof dotRange[key] !=="function"){
-		obj.push(`${rect[key]}`);
-	}
-}
-
-//Assign random values to top and left
-dot.style.top = `${Math.floor(Math.random()*400)}px`;
-dot.style.left = `${Math.floor(Math.random()*400)}px`;
-
-//Point for the event listener
+setInterval(()=>{
+getDot.style.top = `${Math.floor(Math.random()*(getParameters.height))-13}px`;
+getDot.style.left = `${Math.floor(Math.random()*(getParameters.width))-13}px`;
+},5200);
 var point =0;
 
-function Smasher(obj){
-	/**
-	 * Bounding rect height should be used to calculate distance
-	 * Bounding rect width should be used to calculate distance
-	 * ***/
-	const x = obj[0]; const y = obj[1]; const width = obj[2]; const height = obj[3];
-	const topy = obj[4]; const righty = obj[5]; const bottom = obj[6]; const lefty = obj[7];
-}// end function smasher...
+const points = document.createElement("p");
+points.style.top = "0px";
+points.style.left = "100px";
+points.style.backgroundColor = "green";
 
-//EVENT LISTENER
-containerBody.addEventListener("click",(ev)=>{
-	point ++;
-	let x = ev.clientX;
-	let y = ev.clientY;
-	//
-	const maxX = window.innerWidth -rect.width;
-	const maxY = window.innerHeight -rect.height;
+const body = document.querySelector("body");
+body.appendChild(points);
 
-	x = Math.min(Math.max(0,x),maxX);
-	y = Math.min(Math.max(0,y),maxY);
-	
-	dot.style.transform = `translateY(${y}px)`;
-	dot.style.transform += `translateX(${x}px)`;
-	if(point % 2 == 0){
-		dot.style.backgroundColor = "green";
-		points.innerText = point;
-	}else if(point % 2 != 0 ){
-		points.innerText = point;
-	}
-	if(point ===21){
-		points.style.backgroundColor = "red";
-		points.style.fontSize = "75px";
-		points.style.fontFamiliy = "impact";
-		points.style.float = "right";
-		points.style.position = "absolute";
-		points.style.animationDuration = "5s";
-		points.style.animationName = "slide";
-		points.innerText = "GAME OVER!";
-	}
-	
+getDot.addEventListener("click",(e)=>{
+	point+=1;
+	points.innerText = `${point}`;
+	getDot.style.top = `${Math.floor(Math.random()*getParameters1.height)}px`;
+	getDot.style.left = `${Math.floor(Math.random()*getParameters1.width)}px`;
 });
-//Before we trigger this function, assign the top and left value of the dot random values.
-
-//setInterval(Smasher,1500);
-
-
-
 
